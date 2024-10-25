38c57bc5ba64da9b8fa8ba2ff2ca162cab245ce2
Alex Kavvos <alex.kavvos@bristol.ac.uk>
2024-10-25 11:59:37 +0100

    added code to read example from file; removed dead code warnings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::process;
+use std::fs::File;
+use std::io::{self, Read};
 
 use cbpv::translate::translate;
 
@@ -8,25 +11,41 @@ mod cbpv;
 mod eval;
 
 fn main() {
-    let src = "
-cat :: [Nat] -> [Nat] -> [Nat]
-cat xs ys = case xs of
-    [] -> ys.
-    (x:xs) -> x : (cat xs ys).
-
-last :: [Nat] -> Nat
-last xs = exists ys :: [Nat]. exists y :: Nat.
-    cat ys [y] =:= xs. y.
-
-last [1,2,3,4,5,3].
-";
-
     let args: Vec<String> = env::args().collect();
 
-    let solution_count = match args.get(1) {
+     if args.len() > 2 {
+        eprintln!("Error: Expected at most 2 arguments, but got {}.", args.len() - 1);
+        eprintln!("Usage: {} source_file [number of solutions]", args[0]);
+        process::exit(1);
+    }
+
+    let file_name = &args[1];
+    let trials = match args.get(2) {
         Some(n) => n.parse().unwrap(),
         None => 1
     };
+
+    let mut file = match File::open(file_name) {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("Error: Could not open file '{}': {}", file_name, error);
+            process::exit(1);
+        }
+    };
+
+    let mut src = String::new();
+
+    // Try to read the file contents
+    match file.read_to_string(&mut src) {
+        Ok(_) => { interpret(&mut src, trials); }
+        Err(error) => {
+            eprintln!("Error: Could not read file '{}': {}", file_name, error);
+            process::exit(1);
+        }
+    };
+}
+
+fn interpret(src: &mut String, solution_count: usize) {
 
     let ast = parser::parse(src).unwrap();
     let cbpv = translate(ast);
